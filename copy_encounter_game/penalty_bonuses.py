1cf86514cc480fb1951982c7cf1a70197bbee29e
Anton Bogdanov <anton.bogdanov@gameloft.com>
2021-06-17 00:05:06 +0300

    Refactoring

--- copy_encounter_game/penalty_bonuses.py
+++ copy_encounter_game/penalty_bonuses.py
@@ -14,27 +14,35 @@ __all__ = [
 
 
 def penalty_bonuses(
-        right_answer: str, wrong_answers: typing.List[str],
+        right_answers: typing.Set[str],
+        all_answers: typing.List[str],
         levels: typing.List[int],
-        seed: int = None
+        penalty: str = None,
+        seed: int = 41,
+        shuffle_answers: bool = False,
 ) -> typing.Tuple[
         typing.List[Bonus],
         str
 ]:
-    all_answers = [right_answer, *wrong_answers]
-    seed = seed or 41
-    random.seed(seed)
-    random.shuffle(all_answers)
+    to_hash = f"{sorted(right_answers)}{all_answers}{seed}"
+    new_right_ans = hashlib.md5(to_hash.encode()).hexdigest()[:10]
+    wrong_ans_txt = "Неверно"
+    if penalty:
+        wrong_ans_txt = f"{wrong_ans_txt}. Будет начислен штраф {penalty}"
+    right_ans_txt = f"Верно! Введите в поле ввода {new_right_ans!r} (без кавычек)"
+
+    if shuffle_answers:
+        random.seed(seed)
+        random.shuffle(all_answers)
+
     all_bonuses = []
-    to_hash = f"{right_answer}{seed}"
-    new_right_ans = hashlib.md5(to_hash.encode()).hexdigest()[:6]
-    for ans in all_answers:
+    for i, ans in enumerate(all_answers):
         b = Bonus(
-            f"Штрафной бонус",
+            f"Штрафной бонус {i + 1}",
             answers=[ans],
             levels_available=levels,
             bonus_time=(0, 0, 1),
-            hint_text="Неверно" if ans != right_answer else f"Верно! Введите в ответ {new_right_ans}"
+            hint_text=wrong_ans_txt if ans not in right_answers else right_ans_txt
         )
         all_bonuses.append(b)
     return all_bonuses, new_right_ans
